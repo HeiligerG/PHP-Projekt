2931675185d0830dcbf20850fa90402c1433acde
HeiligerG <gggigi111@gmail.com>
2024-12-05 15:58:57 +0100

    Branch strategie change

--- WorkingDir/2024/blogs/gianluca/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<div class=" hidden 2xl:block w-80 p-8 bg-slate-900/50 min-h-[calc(100vh-4rem)] border-l border-slate-800">
-    <div class="sticky top-8">
-        <?php include __DIR__ . '/../components/blj_blogs_sidebar.php'; ?>
-            </div>
-        </div>
